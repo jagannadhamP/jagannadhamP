6a5cd7e65ab79710ba8a03cbdcf05e2c83b74a96
jagan <jagan@gmail.com>
2021-11-26 10:32:51 +0000

    added terraform menifests here

---
+++ main.tf
@@ -0,0 +1,14 @@
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "server2" {
+  ami           = "ami-0ba62214afa52bec7"
+  instance_type = "t2.medium"
+  tags = {
+    Name = "gds-terraform-v2"
+    Env  = "Prod"
+  }
+}
+
